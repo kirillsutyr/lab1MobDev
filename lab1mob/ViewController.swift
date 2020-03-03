e69d454cf9f9ad02e91c64a36498338fb5856f27
Oleksandr Bolshoy <oleksandr@macbook-air-oleksandr.local>
2020-03-03 10:57:34 +0200

    Initial Commit

---
+++ lab1mob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab1mob
+//
+//  Created by Oleksandr Bolshoi on 03.03.2020.
+//  Copyright © 2020 Sutyr Kyrylo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
